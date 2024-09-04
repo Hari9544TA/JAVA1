ccd5d3213aed5571a032ec72ff737c7c7a710e82
Hari9544TA <hariharanr4119@gmail.com>
2024-09-04 14:23:56 +0530

    programs

--- src/SEPT/EX04092024/Lab002.java
+++ src/SEPT/EX04092024/Lab002.java
@@ -3,5 +3,6 @@ package SEPT.EX04092024;
 public class Lab002 {
         public  static void main(String[] args){
             System.out.println("hello world");
+            System.out.println("im a automation tester");
         }
 }
