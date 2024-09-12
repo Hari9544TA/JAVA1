26d3b1300de7da14c0e2b5d9099a835a79522299
Hari9544TA <hariharanr4119@gmail.com>
2024-09-12 11:58:43 +0530

    12 sep

---
+++ src/SEPT/ex12092024/Lab004.java
@@ -0,0 +1,10 @@
+package SEPT.ex12092024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        int a=10, b=20;
+        System.out.println(a>b);
+        System.out.println(a<b);
+
+    }
+}
